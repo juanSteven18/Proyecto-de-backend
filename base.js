fa46b1be3ba09c229664117ac7e41bf1605b11c3
JuanS183 <reateguiazuajejs@uvm.edu.ve>
2026-05-12 11:56:38 -0400

    se modifico el archivo base de datos para incluir fechas

--- base.js
+++ base.js
@@ -1,8 +1,8 @@
 let productos = [
-    { id: 1, nombre: "Producto Base A", categoria: "General" },
-    { id: 2, nombre: "Producto Base B", categoria: "General" },
-    { id: 3, nombre: "Producto Base C", categoria: "General" },
-    { id: 4, nombre: "Producto Base D", categoria: "General" }
+    { id: 1, nombre: "Producto Base A", categoria: "General",fecha: "2026-05-01" },
+    { id: 2, nombre: "Producto Base B", categoria: "General", fecha: "2026-05-02"},
+    { id: 3, nombre: "Producto Base C", categoria: "General", fecha: "2026-05-03"},
+    { id: 4, nombre: "Producto Base D", categoria: "General", fecha: "2026-05-04"}
 ];
 
 
